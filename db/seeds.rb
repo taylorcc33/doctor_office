98558c28fd38c9d959f1fe832d284a33fab35e90
Taylor Collins <taylorcc33@gmail.com>
2020-11-20 14:59:38 -0700

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+patients = []
+doctors = []
+
+
+
+30.times do |i|
+  patients << Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+3.times do |i|
+  doctors << Doctor.create(name: Faker::TvShows::TwinPeaks.character, specialty: Faker::Game.platform)
+end
+
+doctors.each do |doctor|
+  10.times do
+    doctor.appointments.create(date: Faker::Date.between(from: '2020-10-23', to: '2020-12-31'), patient_id: patients.sample.id)
+  end
+end
+
+puts "seeded"
+puts Doctor.all.size
+puts Patient.all.size
+puts Appointment.all.size
+
